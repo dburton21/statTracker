3c96ef7cb74abfcf3722466cd2071a890aaca656
david burton <davidburtonct@edge-edtech.local>
2020-05-19 11:02:17 -0400

    merged MVP

--- BasketballTracker/ViewController.swift
+++ BasketballTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-
+import SQLite3
 
 
 class ViewController: UIViewController {
@@ -48,6 +48,47 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 //        circularButton?.mybg(_bg:.red)
         // Do any additional setup after loading the view, typically from a nib.
+        var db: OpaquePointer?
+        
+        let fileUrl = try!
+            FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("BasketballStats.sqlite")
+        
+        if sqlite3_open(fileUrl.path, &db) != SQLITE_OK{
+            print("Error opening database.")
+            return
+        }
+        
+        let createTableQuery = "CREATE TABLE statsf (id INTEGER PRIMARY KEY, name TEXT, threesMade INTEGER, threestaken INTEGER, threepercent INTEGER, twosmade INTEGER, twostaken INTEGER, twopercent INTEGER, freemade INTEGER, freetaken INTEGER, freepercent INTEGER, stls INTEGER, rebs INTEGER, blks INTEGER, asts INTEGER, turno INTEGER, chg INTEGER, rate INTEGER)"
+        
+        if sqlite3_exec(db, createTableQuery, nil, nil, nil) != SQLITE_OK{
+            print("Error creating table.")
+            return
+        }
+        
+        print("Table created.")
+        
+        
+        
+        let insertTableQuery = "INSERT INTO statsf (id, threesMade) VALUES (2, 5)"
+        
+        if sqlite3_exec(db, insertTableQuery, nil, nil, nil) != SQLITE_OK{
+            print("Error inserting item.")
+            return
+        }
+        
+        print("Item inserted.")
+        
+        let selectTableQuery = "SELECT threesMade FROM statsf WHERE id = 2"
+        
+        var queryStatement: OpaquePointer?
+        if sqlite3_prepare_v2(db, selectTableQuery, -1, &queryStatement, nil) == SQLITE_OK {
+            if sqlite3_step(queryStatement) == SQLITE_ROW {
+                let threes = sqlite3_column_int(queryStatement, 0)
+                print("\(threes)")
+            }
+        }
+        
+        
         print("It opened...")
     }
 
@@ -202,6 +243,7 @@ class ViewController: UIViewController {
             print("Please pick a player first.")
         }
     }
+    
     
 }
 
--- BasketballTracker/dataBase.swift
+++ BasketballTracker/dataBase.swift
@@ -1,59 +1,59 @@
+////
+////  dataBase.swift
+////  BasketballTracker
+////
+////  Created by david burton on 5/19/20.
+////  Copyright © 2020 Burton inc. All rights reserved.
+////
 //
-//  dataBase.swift
-//  BasketballTracker
+//import Foundation
 //
-//  Created by david burton on 5/19/20.
-//  Copyright © 2020 Burton inc. All rights reserved.
+//import UIKit
+//import SQLite3
+//
+//
+//
+//
+//    var db: OpaquePointer?
+//
+//        let fileUrl = try!
+//            FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("BasketballStats.sqlite")
+//
+//        if sqlite3_open(fileUrl.path, &db) != SQLITE_OK{
+//            print("Error opening database.")
+//            return
+//        }
+//
+//        let createTableQuery = "CREATE TABLE statsf (id INTEGER PRIMARY KEY, name TEXT, threesMade INTEGER, threestaken INTEGER, threepercent INTEGER, twosmade INTEGER, twostaken INTEGER, twopercent INTEGER, freemade INTEGER, freetaken INTEGER, freepercent INTEGER, stls INTEGER, rebs INTEGER, blks INTEGER, asts INTEGER, turno INTEGER, chg INTEGER, rate INTEGER)"
+//
+//        if sqlite3_exec(db, createTableQuery, nil, nil, nil) != SQLITE_OK{
+//            print("Error creating table.")
+//            return
+//        }
+//
+//        print("Table created.")
+//
+//
+//
+//        let insertTableQuery = "INSERT INTO statsf (id, threesMade) VALUES (2, 5)"
+//
+//        if sqlite3_exec(db, insertTableQuery, nil, nil, nil) != SQLITE_OK{
+//            print("Error inserting item.")
+//            return
+//        }
+//
+//        print("Item inserted.")
+//
+//        let selectTableQuery = "SELECT threesMade FROM statsf WHERE id = 2"
+//
+//        var queryStatement: OpaquePointer?
+//        if sqlite3_prepare_v2(db, selectTableQuery, -1, &queryStatement, nil) == SQLITE_OK {
+//            if sqlite3_step(queryStatement) == SQLITE_ROW {
+//                let threes = sqlite3_column_int(queryStatement, 0)
+//                print("\(threes)")
+//            }
+//        }
+//
 //
 
-import Foundation
-
-import UIKit
-import SQLite3
-
-
-class dataBase: UIView {
-    
-    var db: OpaquePointer?
-    
-        let fileUrl = try!
-            FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("BasketballStats.sqlite")
-        
-        if sqlite3_open(fileUrl.path, &db) != SQLITE_OK{
-            print("Error opening database.")
-            return
-        }
-        
-        let createTableQuery = "CREATE TABLE statsf (id INTEGER PRIMARY KEY, name TEXT, threesMade INTEGER, threestaken INTEGER, threepercent INTEGER, twosmade INTEGER, twostaken INTEGER, twopercent INTEGER, freemade INTEGER, freetaken INTEGER, freepercent INTEGER, stls INTEGER, rebs INTEGER, blks INTEGER, asts INTEGER, turno INTEGER, chg INTEGER, rate INTEGER)"
-        
-        if sqlite3_exec(db, createTableQuery, nil, nil, nil) != SQLITE_OK{
-            print("Error creating table.")
-            return
-        }
-        
-        print("Table created.")
-        
-        
-        
-        let insertTableQuery = "INSERT INTO statsf (id, threesMade) VALUES (2, 5)"
-        
-        if sqlite3_exec(db, insertTableQuery, nil, nil, nil) != SQLITE_OK{
-            print("Error inserting item.")
-            return
-        }
-        
-        print("Item inserted.")
-        
-        let selectTableQuery = "SELECT threesMade FROM statsf WHERE id = 2"
-        
-        var queryStatement: OpaquePointer?
-        if sqlite3_prepare_v2(db, selectTableQuery, -1, &queryStatement, nil) == SQLITE_OK {
-            if sqlite3_step(queryStatement) == SQLITE_ROW {
-                let threes = sqlite3_column_int(queryStatement, 0)
-                print("\(threes)")
-            }
-        }
-        
-        
-    }
 
